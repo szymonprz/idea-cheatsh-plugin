acc0dc74ff33db4dda7c473172c548efa86710b5
Szymon Przebierowski <szymonprz@gmail.com>
2019-02-04 22:57:33 +0100

    don't include comments by default

--- src/test/groovy/pl/szymonprz/cheatsh/plugin/question/QuestionBuilderTest.groovy
+++ src/test/groovy/pl/szymonprz/cheatsh/plugin/question/QuestionBuilderTest.groovy
@@ -9,30 +9,35 @@ class QuestionBuilderTest extends Specification {
         question(input) == result
         where:
         input           | result
-        "text text"     | "text+text"
-        "text\ttext"    | "text+text"
-        "text\ntext"    | "text+text"
+        "text text"     | "text+text?Q"
+        "text\ttext"    | "text+text?Q"
+        "text\ntext"    | "text+text?Q"
     }
 
     def "should specify context from question"() {
         expect:
-        question("java/my question") == "java/my+question"
-        question("java/my question/extra data") == "java/my+question/extra+data"
+        question("java/my question") == "java/my+question?Q"
+        question("java/my question/extra data") == "java/my+question/extra+data?Q"
     }
 
     def "should specify context from extension when not given in question"() {
         expect:
-        question("my question", "kt") == "kotlin/my+question"
+        question("my question", "kt") == "kotlin/my+question?Q"
     }
 
     def "should not specify context from invalid extension and without context in question"() {
         expect:
-        question("my question", "invalidExt") == "my+question"
+        question("my question", "invalidExt") == "my+question?Q"
     }
 
     def "should specify question even when extension is valid"() {
         expect:
-        question("java/my question", "kt") == "java/my+question"
+        question("java/my question", "kt") == "java/my+question?Q"
+    }
+
+    def "should ignore comments by default"() {
+        expect:
+        question("java/my question", "kt") == "java/my+question?Q"
     }
 
     String question(String text){
